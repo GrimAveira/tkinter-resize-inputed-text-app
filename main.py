315db47e52f5ec8bfd232a1d8c11c3ea00f35306
RagnNotebook <st.tryed@gmail.com>
2023-09-25 19:09:41 +0400

    The application was wrapped in a class

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
 from tkinter import *
- 
 
-fontSize=10
+class MyApp():
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("METANIT.COM")
+        self.root.geometry("850x850")
+        self.fontSize=10
+        self.message = StringVar()
+        self.xLabel=Label(text="Координата x")
+        self.xLabel.pack()
+        self.xEntry = Entry(width=20)
+        self.xEntry.pack()
+        self.yLabel=Label(text="Координата y")
+        self.yLabel.pack()
+        self.yEntry = Entry(width=20)
+        self.yEntry.pack()
+        self.accept=Button(self.root, text="Применить координаты", command=self.acceptCord)
+        self.accept.pack()
+        self.textLabel=Label(text="Выводимый текст")
+        self.textLabel.pack()
+        self.text = Entry(width=20, textvariable=self.message)
+        self.text.pack()
+        self.labelOut = Label(self.root, borderwidth=1, relief="solid")
+        self.labelOut.pack(padx=0, pady=0,anchor='nw')
+        self.label = Label(self.labelOut, textvariable=self.message, borderwidth=1, relief="solid",
+            font='Times 10')
+        self.label.pack(padx=5, pady=5)
+        self.root.bind('<Key>',self.keypress)
+    def playApp(self):
+        self.root.mainloop()
+    def keypress(self, event):
+        self.fontSize
+        if event.keycode==27:
+            self.root.destroy()
+        if event.keycode==37:
+            self.fontSize/=2
+            self.label['font']= 'Times '+str(int(self.fontSize))
+        if event.keycode==39:
+            self.fontSize*=2
+            self.label['font']= 'Times '+str(int(self.fontSize))
+    def acceptCord(self):
+        self.labelOut.pack(padx=self.xEntry.get(), pady=self.yEntry.get(),anchor='nw')
 
-root = Tk()
-root.title("METANIT.COM")
-root.geometry("850x850")
-message = StringVar()
-
-
-def keypress(event):
-    global fontSize
-    if event.keycode==37:
-        fontSize/=2
-        label['font']= 'Times '+str(int(fontSize))
-    if event.keycode==39:
-        fontSize*=2
-        label['font']= 'Times '+str(int(fontSize))
-        
-def acceptCord():
-    labelOut.pack(padx=xEntry.get(), pady=yEntry.get(),anchor='nw')
-
-textLabel=Label(text="Выводимый текст")
-textLabel.pack()
-text = Entry(width=20, textvariable=message)
-text.pack()
-xLabel=Label(text="Координата x")
-xLabel.pack()
-xEntry = Entry(width=20)
-xEntry.pack()
-yLabel=Label(text="Координата y")
-yLabel.pack()
-yEntry = Entry(width=20)
-yEntry.pack()
-accept=Button(root, text="Применить координаты", command=acceptCord)
-accept.pack()
-
-
-root.bind('<Key>',keypress)
-
-labelOut = Label(root, borderwidth=1, relief="solid")
-labelOut.pack(padx=0, pady=0,anchor='nw')
-label = Label(labelOut, textvariable=message, borderwidth=1, relief="solid",
-    font='Times 10')
-label.pack(padx=5, pady=5)
- 
-root.mainloop()
+app=MyApp()
+app.playApp()
