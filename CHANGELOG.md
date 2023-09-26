14bf6303f6cb671b1d5c6441f77a03a5d9127718
RagnNotebook <st.tryed@gmail.com>
2023-09-26 19:25:32 +0400

    Formated by PIP8 and expansion of functionality

---
+++ CHANGELOG.md
@@ -0,0 +1,33 @@
+# Change log
+
+## 0.0.3 (26-09-2023)
+
+### Created
+
+- gitignore
+- CHANGELOG
+- Functions:
+  - apply_coordinates
+  - double_apply_coordinates
+  - resize_font_size
+  - entry_handler
+
+### Fixed
+
+- Code in `main.py` was divided into logical blocks and formated by PEP8 format
+
+### REMOVED
+
+## 0.0.2 (25-09-2023)
+
+### Fixed
+
+- The application was wrapped in a class
+
+## 0.0.1 (25-09-2023)
+
+### Added
+
+- README
+- LICENSE
+- main.py
--- main.py
+++ main.py
@@ -2,45 +2,71 @@ from tkinter import *
 
 class MyApp():
     def __init__(self):
-        self.root = Tk()
-        self.root.title("METANIT.COM")
-        self.root.geometry("850x850")
-        self.fontSize=10
-        self.message = StringVar()
-        self.xLabel=Label(text="Координата x")
-        self.xLabel.pack()
-        self.xEntry = Entry(width=20)
-        self.xEntry.pack()
-        self.yLabel=Label(text="Координата y")
-        self.yLabel.pack()
-        self.yEntry = Entry(width=20)
-        self.yEntry.pack()
-        self.accept=Button(self.root, text="Применить координаты", command=self.acceptCord)
-        self.accept.pack()
-        self.textLabel=Label(text="Выводимый текст")
-        self.textLabel.pack()
-        self.text = Entry(width=20, textvariable=self.message)
-        self.text.pack()
-        self.labelOut = Label(self.root, borderwidth=1, relief="solid")
-        self.labelOut.pack(padx=0, pady=0,anchor='nw')
-        self.label = Label(self.labelOut, textvariable=self.message, borderwidth=1, relief="solid",
+        self._root = Tk()
+        self._root.title("METANIT.COM")
+        self._root.geometry("450x450")
+
+        self._fontSize=10
+        self._message = StringVar()
+
+        self._textLabel=Label(text="Выводимый текст")
+        self._textLabel.pack()
+        self._outputText = Entry(width=20, textvariable=self._message)
+        self._outputText.pack()
+        self._xLabel=Label(text="Координата x")
+        self._xLabel.pack()
+        self._xEntry = Entry(width=20)
+        self._xEntry.pack()
+        self._yLabel=Label(text="Координата y")
+        self._yLabel.pack()
+        self._yEntry = Entry(width=20)
+        self._yEntry.pack()
+        self._acceptButton=Button(self._root, text="Применить координаты", command=self.double_apply_coordinates)
+        self._acceptButton.pack()
+        self._externalLabel = Label(self._root, borderwidth=1, relief="solid")
+        self._externalLabel.pack(padx=0, pady=0,anchor='nw')
+        self._innerLabel = Label(self._externalLabel, textvariable=self._message, borderwidth=1, relief="solid",
             font='Times 10')
-        self.label.pack(padx=5, pady=5)
-        self.root.bind('<Key>',self.keypress)
-    def playApp(self):
-        self.root.mainloop()
-    def keypress(self, event):
-        self.fontSize
-        if event.keycode==27:
-            self.root.destroy()
+        self._innerLabel.pack(padx=5, pady=5)
+
+        self._root.bind('<Escape>', self.close_app)
+        self._root.bind('<Key>', self.resize_font_size)
+
+    def entry_handler(self, value):
+        if int (value)<0:
+            raise ValueError()
+        
+    def start_app(self):
+        self._root.mainloop()
+
+    def close_app(self, _):
+        self._root.destroy()
+
+    def resize_font_size(self, event):
         if event.keycode==37:
-            self.fontSize/=2
-            self.label['font']= 'Times '+str(int(self.fontSize))
+            self._fontSize/=2
+            self._innerLabel['font']= 'Times '+str(int(self._fontSize))
         if event.keycode==39:
             self.fontSize*=2
-            self.label['font']= 'Times '+str(int(self.fontSize))
-    def acceptCord(self):
-        self.labelOut.pack(padx=self.xEntry.get(), pady=self.yEntry.get(),anchor='nw')
+            self._innerLabel['font']= 'Times '+str(int(self._fontSize))
+
+    def double_apply_coordinates(self):
+        self.apply_coordinates()
+        self.apply_coordinates()
+
+    def apply_coordinates(self):
+        try:
+            x=self._xEntry.get()
+            y=self._yEntry.get()
+            self.entry_handler(x)
+            self.entry_handler(y)
+            self._externalLabel.pack(padx=x, pady=y,anchor='nw')
+            self._innerLabel.pack(padx=5, pady=5)
+            self._innerLabel['text']=self._message
+            self._innerLabel.update()
+        except ValueError:
+            print("Недопустимое значение координат")
+
 
 app=MyApp()
-app.playApp()
+app.start_app()
